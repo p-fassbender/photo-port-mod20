ed6f779a7bb85dc416dd3df421d0e3eeb3974bb2
Preston Fassbender <fassbenderp0551@gmail.com>
2022-04-13 13:28:49 -0500

    gallery and photolist tests

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -3,12 +3,12 @@ import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery({ currentCategory }) {
-    const { name, description } = currentCategory;
+    const { name, description } = currentCategory; // destructure props even more, but currentCategory.name and currentCategory.description work too
     return (
         <section>
             <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
             <p>{description}</p>
-            <PhotoList category={currentCategory.name} />
+            <PhotoList category={name} />
         </section>
     );
 }
